a10b8992a159ee936a4c903892aede575c7d1f79
NeverMine17 <dannevergame@gmail.com>
2017-09-23 16:04:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,71 +7,73 @@ int main()
     {
     txCreateWindow (1200, 700);
 
-    int Q;
-    cout << "1-McLaren P1   2-Ferrari Enzo  3-Gazel Next" << endl;
+    char* Q;
+    cout << "McLaren P1   Ferrari Enzo  Gazel Next" << endl;
     cin >> Q ;
 
-    switch (Q) {
-    case 1:
+    if (Q == "McLaren P1")
+        {
         HDC fon1 = txLoadImage ("M.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon1, 0, 0);
-        break;
-    case 2:
+        }
+    else if (Q == "Ferrari Enzo")
+        {
         HDC fon2 = txLoadImage ("F.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon2, 0, 0);
-        break;
-    case 3:
+        }
+    else if (Q == "Gazel Next")
+        {
         HDC fon3 = txLoadImage ("g.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon3, 0, 0);
-        break;
-    default:
-        exit(1);
-    }
-    
+        }
 
-    int C;
-    cout << "1-Blue   2-Red  3-White" << endl;
-    cin >> C;
+    char* Q;
+    cout << "Blue  Red  White" << endl;
+    cin >> Q ;
 
-    switch (C) {
-    case 1:
+    if (Q == "Blue")
+        {
         HDC fon1 = txLoadImage ("M.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon1, 0, 0);
-        break;
-    case 2:
+        }
+    else if (Q == "Red")
+        {
         HDC fon2 = txLoadImage ("F.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon2, 0, 0);
-        break;
-    case 3:
+        }
+    else if (Q == "White")
+        {
         HDC fon3 = txLoadImage ("g.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon3, 0, 0);
-        break;
-    default:
-        exit(1);
-    }
+        }
 
-    int S;
-    cout << "1-Little   2-Big  3-None" << endl;
-    cin >> S;
 
-    switch (S) {
-    case 1:
+    char* Q;
+    cout << "Spoiler Little  Big  None" << endl;
+    cin >> Q ;
+
+    if (Q == "Little")
+        {
         HDC fon1 = txLoadImage ("M.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon1, 0, 0);
-        break;
-    case 2:
+        }
+    else if (Q == "Big")
+        {
         HDC fon2 = txLoadImage ("F.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon2, 0, 0);
-        break;
-    case 3:
+        }
+    else if (Q == "None")
+        {
         HDC fon3 = txLoadImage ("g.bmp");
         txBitBlt (txDC(), 0, 0, 1200, 700, fon3, 0, 0);
-        break;
-    default:
-        exit(1);
-    }
+        }
+
+
     HDC sp = txLoadImage ("sp.bmp");
     txBitBlt (txDC(), 200, 0, 200, 200, sp, 0, 0);
 
+
+
+
     return 0;
-}
+    }
